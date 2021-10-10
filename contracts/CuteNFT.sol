30beb833efd15830bdea73c94de8c9945d428b72
ybingcheng <ybingcheng@gmail.com>
2021-10-10 11:56:36 +0800

    add start stop to mint, mint per tx, price per mint

--- contracts/CuteNFT.sol
+++ contracts/CuteNFT.sol
@@ -10,19 +10,54 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 contract CuteNFT is ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
+    event MintNft(address indexed sender, uint256 startWith, uint256 times);
+
+    // supply counters
+    uint256 public totalNfts;
+    uint256 public totalCount = 9999;
+
+    // max mint per tx
+    uint256 public maxBatch = 10;
+
+    // price per mint 0.05E
+    uint256 public price = 5000000000000000;
+
+    // mint start
+    bool private started;
 
     constructor() public ERC721("CuteNFT", "CUTE") {}
 
-    function mintNFT(address recipient, string memory tokenURI)
-        public onlyOwner
-        returns (uint256)
-    {
+    function setStart(bool _start) public onlyOwner {
+        started = _start;
+    }
+
+    function mintNFT(
+        address recipient,
+        string memory tokenURI,
+        uint256 _times
+    ) payable public {
+        require(started, "mint not started");
+
+        require(
+            _times > 0 && _times <= maxBatch,
+            "exceed max mint per tx"
+        );
+        require(totalNfts + _times <= totalCount, "mint over!");
+
+        require(msg.value == _times * price, "value error, please check price.");
+        payable(owner()).transfer(msg.value);
+
+
         _tokenIds.increment();
 
         uint256 newItemId = _tokenIds.current();
         _mint(recipient, newItemId);
         _setTokenURI(newItemId, tokenURI);
 
-        return newItemId;
+        emit MintNft(_msgSender(), totalNfts+1, _times);
+
+        for(uint256 i=0; i< _times; i++){
+            _mint(_msgSender(), 1 + totalNfts++);
+        }
     }
 }
