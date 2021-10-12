42095c98ac11c625ca903c4dc95a48c8a5beaee7
Bing <ybingcheng@gmail.com>
2021-10-12 14:01:27 +0800

    Feat/mint details (#1)
    * add start stop to mint, mint per tx, price per mint
    
    * Add withdrawal for dev
    
    * Use string for url
    
    * Add etherscan
    
    * update mint
    
    * Change encode

--- contracts/CuteNFT.sol
+++ contracts/CuteNFT.sol
@@ -8,21 +8,80 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract CuteNFT is ERC721URIStorage, Ownable {
+    using Strings for uint256;
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    constructor() public ERC721("CuteNFT", "CUTE") {}
+    event MintNft(address indexed sender, uint256 startWith, uint256 times);
+    event BaseURIChanged(string baseURI);
 
-    function mintNFT(address recipient, string memory tokenURI)
-        public onlyOwner
-        returns (uint256)
-    {
-        _tokenIds.increment();
+    // supply counters
+    uint256 public constant totalSupply = 10000; // etherscan requirement
+    uint256 public constant MAX_PER_MINT = 10;
+    uint256 public constant PRICE = 5000000000000000; // price per mint 0.005E
+    address public constant devAddress =
+        0x6D173418E4D31C4f000098C4d95Ff737AC133C7a;
 
-        uint256 newItemId = _tokenIds.current();
-        _mint(recipient, newItemId);
-        _setTokenURI(newItemId, tokenURI);
+    uint256 public numTokensMinted;
 
-        return newItemId;
+    // mint start
+    bool private started;
+
+    string public baseTokenURI;
+
+    // Set baseUrl during constructor?
+    constructor(string memory initialURI) public ERC721("CuteNFT", "CUTE") {
+        baseTokenURI = initialURI;
+    }
+
+    function setStart(bool _start) public onlyOwner {
+        started = _start;
+    }
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return baseTokenURI;
+    }
+
+    function setBaseURI(string memory baseURI) public onlyOwner {
+        baseTokenURI = baseURI;
+        emit BaseURIChanged(baseURI);
+    }
+
+    function mint(uint256 _times) public payable {
+        require(started, "mint not started");
+
+        require(_times > 0 && _times <= MAX_PER_MINT, "incorrect mint number");
+        require(numTokensMinted + _times <= totalSupply, "mint over!");
+
+        require(
+            msg.value == _times * PRICE,
+            "value error, please check price."
+        );
+        // pay direct to contract owner
+        payable(owner()).transfer(msg.value);
+
+        for (uint256 i = 0; i < _times; i++) {
+            _tokenIds.increment();
+
+            uint256 newItemId = _tokenIds.current();
+            _safeMint(_msgSender(), 1 + numTokensMinted++);
+            _setTokenURI(
+                newItemId,
+                string(abi.encodePacked(newItemId.toString(), ".json"))
+            );
+
+            emit MintNft(_msgSender(), totalSupply + 1, _times);
+        }
+    }
+
+    function withdrawAll() public onlyOwner {
+        uint256 balance = address(this).balance;
+        require(balance > 0, "Insufficent balance");
+        _withdraw(devAddress, balance);
+    }
+
+    function _withdraw(address _address, uint256 _amount) private {
+        (bool success, ) = _address.call{value: _amount}("");
+        require(success, "Failed to withdraw Ether");
     }
 }
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,14 +1,17 @@
 require('dotenv').config();
 
-import "@nomiclabs/hardhat-ethers";
-import "@nomiclabs/hardhat-etherscan";
+import '@nomiclabs/hardhat-ethers';
+import '@nomiclabs/hardhat-etherscan';
+import '@nomiclabs/hardhat-waffle';
+import '@typechain/hardhat';
 
 const ALCHEMY_KEY = process.env.ALCHEMY_KEY || '';
 const PRIVATE_KEY = process.env.PRIVATE_KEY || '';
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || '';
 
 module.exports = {
   solidity: "0.8.0",
-  defaultNetwork: "ropsten",
+  defaultNetwork: "hardhat",
   networks: {
     hardhat: {},
     ropsten: {
@@ -16,5 +19,17 @@ module.exports = {
       accounts: [`0x${PRIVATE_KEY}`],
       chainId: 3,
     },
+    mumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${ALCHEMY_KEY}`,
+      accounts: [`0x${PRIVATE_KEY}`],
+      chainId: 80001,
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  typechain: {
+    outDir: 'typechain',
+    target: 'ethers-v5',
   },
 };
