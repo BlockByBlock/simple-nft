611edca82848297bb1c99fe67d65bbb0c4a53ffd
ybingcheng <ybingcheng@gmail.com>
2021-10-14 02:34:43 +0800

    Add test to mint

--- test/CuteNFT.spec.ts
+++ test/CuteNFT.spec.ts
@@ -15,16 +15,52 @@ describe('CuteNFT', () => {
 
     const cuteNftFactory = new CuteNFT__factory(owner);
     cuteNftContract = await cuteNftFactory.deploy('https://example.com/');
-
-    // start mint
-    await cuteNftContract.connect(owner).setStart(true);
   });
 
   describe('mint', async () => {
-    it('should successfully mint', async () => {      
+    it('should not mint as contract start is false', async () => {
+      await expect(
+        cuteNftContract.connect(minter).mint(1, { value: ethers.utils.parseEther('0.005') }),
+      ).to.revertedWith('mint not started');
+    });
+
+    it('should successfully mint as a minter', async () => {
+      // start mint
+      await cuteNftContract.connect(owner).setStart(true);
+
       await expect(
         await cuteNftContract.connect(minter).mint(1, { value: ethers.utils.parseEther('0.005') }),
       ).to.emit(cuteNftContract, 'MintNft');
     });
+
+    it('should not mint as a minter with insufficient ether', async () => {
+      await expect(
+        cuteNftContract.connect(minter).mint(1, { value: ethers.utils.parseEther('0.0049') }),
+      ).to.revertedWith('value error, please check price');
+    });
+
+    it('should not mint as a minter with insufficient ether for 2', async () => {
+      await expect(
+        cuteNftContract.connect(minter).mint(2, { value: ethers.utils.parseEther('0.005') }),
+      ).to.revertedWith('value error, please check price');
+    });
+
+    it('should not mint as a minter with 0 token', async () => {
+      await expect(
+        cuteNftContract.connect(minter).mint(0, { value: ethers.utils.parseEther('0.005') }),
+      ).to.revertedWith('incorrect mint number');
+    });
+
+    it('should pay owner for 1 mint', async () => {
+      const beforeMintBalance = await owner.getBalance();
+      await cuteNftContract.connect(minter).mint(1, { value: ethers.utils.parseEther('0.005') });
+      expect(await owner.getBalance()).to.be.eq(beforeMintBalance.add(ethers.utils.parseEther('0.005')));
+    });
   });
+
+  // transfer token
+  // transfer
+
+  // retrieve baseUrl
+  // get totalsupply
 });
